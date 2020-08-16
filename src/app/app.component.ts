e8d8ab47a00a7c17a293feef6875f1a5827bc158
panvicka <panvicka@gmail.com>
2020-08-16 12:14:36 +0200

    buttons, spinners, icons, datepickers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+
+ 
+
 
 @Component({
   selector: 'app-root',
@@ -8,12 +12,56 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'material-test';
   isChecked = true;
+  progress = 0;
+  progressAuto = 0;
+  timer;
+  demoData;
+
+
+  constructor() {
+
+    
+
+    this.timer = setInterval(() => {
+      this.progressAuto++;
+      if (this.progressAuto == 100) clearInterval(this.timer);
+    }, 20);
+
+  }
+
   colors = [
     { id: 1, name: 'red' },
     { id: 2, name: 'green' },
     { id: 3, name: 'blue' },
   ]
   color = 3;
+
+  minDate = new Date(2017, 1, 1);
+  maxDate = new Date(2020, 12, 12);
+
+  categories = [
+    { name: 'begginer', selected: false },
+    { name: 'intermediate', selected: false },
+    { name: 'advanced', selected: false }
+  ]
+  selectCategory(category) {
+    this.categories
+      .filter(c => c != category)
+      .forEach(c => c['selected'] = false);
+
+    category.selected = !category.selected;
+  }
+
+  addProgress() {
+    if (this.progress < 100) {
+      this.progress++
+    } else {
+      this.progress = 0;
+    }
+
+  }
+
+ 
 
 
   onChange($event) {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,16 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatSelectModule } from '@angular/material/select';
 import {MatInputModule} from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
   declarations: [
@@ -23,7 +30,15 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
     MatRadioModule,
     MatSelectModule,
     FormsModule,
-    MatInputModule
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatIconModule,
+    MatButtonModule,
+    MatChipsModule,
+    MatProgressSpinnerModule,
+    MatTooltipModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
